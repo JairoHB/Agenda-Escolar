d487eb4e5a776902da2b635c2ca844a64302b0ec
JairoHB <jairotorreshb@outlook.com>
2018-09-25 17:59:55 -0500

    Login

--- app/src/main/java/com/jairohb/agenda_escolar/login.java
+++ app/src/main/java/com/jairohb/agenda_escolar/login.java
@@ -1,12 +1,28 @@
 package com.jairohb.agenda_escolar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class login extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+    }
+
+    public void registeractivity(View v){
+        TextView register = (TextView) findViewById(R.id.txtregister);
+        register.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(login.this, register.class);
+                startActivity(intent);
+            }
+        });
     }
 }
